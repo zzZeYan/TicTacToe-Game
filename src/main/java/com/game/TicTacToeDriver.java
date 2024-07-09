722e62154ccd0887689dff3390104d7fcf7f72ee
Dapeng-Gan <Gandapeng040806@126.com>
2024-07-09 20:30:39 +0800

    Implement TicTacToe and TicTacToeDriver class, and make the game process can be started and finished normally

--- src/main/java/com/game/TicTacToeDriver.java
+++ src/main/java/com/game/TicTacToeDriver.java
@@ -1,8 +1,33 @@
 package com.game;
 
+import java.util.Scanner;
+
 /**
  * Driver class of the TicTacToe game, which is also the entry of the game
  */
 public class TicTacToeDriver {
 
+    public static void entry() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            // Start a new game
+            System.out.println("Welcome to join the TicTacToe game!");
+            TicTacToe ticTacToe = new TicTacToe();
+            ticTacToe.start();
+
+            // User can select continue playing the game
+            System.out.print("Do you want to start a new game [Y or N]? ");
+            String choice = scanner.nextLine();
+            if (choice.equals("N")) {
+                System.out.println("Thank you for playing the TicTacToe game, bye!");
+                break;
+            } else {
+                System.out.print("\n");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        entry();
+    }
 }
